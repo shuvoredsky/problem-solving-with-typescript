6774aa496e490fe775db53f00fff430f624f7adf
shuvo chakrabrati <kumarshuvo265@gmail.com>
2025-11-17 00:25:02 +0600

    problem two solve

--- solution.ts
+++ solution.ts
@@ -1,12 +1,4 @@
-// Problem 1:
-// Create a function formatValue that accepts a value which may be a string, number, or boolean, and returns the following based on the value type:
 
-// If the input is a string → return the string in uppercase
-// If the input is a number → return the number multiplied by 10
-// If the input is a boolean → return the opposite value (true → false, false → true)
-// Requirements:
-// You must write the correct type for the function parameter and the return type.
-// You must use type checking to handle each case.
 
 const formatValue = (value: string | number | boolean): string | number | boolean =>{
     if(typeof value === "string"){
@@ -20,3 +12,22 @@ const formatValue = (value: string | number | boolean): string | number | boolea
     return value;
 }
 
+// Problem 2:
+// Create a function getLength that accepts a value which may be a string or an array, and returns the length of the value.
+
+// If the input is a string → return the number of characters.
+// If the input is an array → return the number of elements.
+// Requirements:
+// You must write the correct type for the function parameter and the return type.
+// You must use type checking to handle each case (typeof or Array.isArray).
+
+const getLength = (value: string | any[])=>{
+    if(typeof value === "string"){
+        return value.length;
+    }else if(Array.isArray(value)){
+        return value.length;
+    }
+}
+
+console.log(getLength('typescript'));
+console.log(getLength([10, 20, 30, 40]));
