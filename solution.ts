c0a6f3f67d8c8c06197a60c6074acb6ec3c7ba18
shuvo chakrabrati <kumarshuvo265@gmail.com>
2025-11-17 22:43:57 +0600

    problem 4 solve

--- solution.ts
+++ solution.ts
@@ -32,9 +32,37 @@ class Person  {
         this.age = age;
     }
 
-    getDetails(){
+    getDetails(): string{
         return `Name: ${this.name}, Age:${this.age}`;
     } 
 }
 
+// Create a function filterByRating that accepts an array of items, where each item has the following properties:
 
+// title (string)
+// rating (number between 0 and 5)
+// The function should return a new array containing only the items with a rating of 4 or more.
+
+// Requirements:
+// You must write the correct type for the function parameter and the return type.
+// Do not mutate the original array.
+// Sample Input:
+
+type Item = {
+    title: string
+    rating: number
+}
+
+const filterByRating = (value: Item[])=>{
+    return value.filter(item=> item.rating >=4 )
+     
+}
+
+
+const books = [
+  { title: 'Book A', rating: 3.9 },
+  { title: 'Book B', rating: 4.2 },
+  { title: 'Book C', rating: 4 },
+];
+
+console.log(filterByRating(books));
